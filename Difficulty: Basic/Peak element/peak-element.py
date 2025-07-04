b7d5bf1fc9a4bf7b3fcf6136195b863ddaa85ac5
KRIZ <krizofficial.index@gmail.com>
2025-07-04 15:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.py
@@ -0,0 +1,15 @@
+class Solution:   
+    def peakElement(self,arr):
+        # Code here
+        left = 0
+        right = len(arr) - 1
+        
+        while(left < right):
+            
+            mid = (left + right) // 2
+
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
